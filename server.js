528ec4dc57b13a10d9764c02f283a53d3fb23c1e
Ryzen <zilckiqgames@hotmail.com>
2019-10-26 12:12:04 +0800

    1

--- server.js
+++ server.js
@@ -20,20 +20,20 @@ app
     .use(router.routes())
     .use(router.allowedMethods());
 
+router.get('/', async (ctx) => {
+    ctx.redirect('index.html');
+});
+
+router
+    .use('/users', users);
+
 const SSL = {
     key: fs.readFileSync('./ssl/harmonious.tech.key'),  //ssl文件路径
     cert: fs.readFileSync('./ssl/harmonious.tech.pem')  //ssl文件路径
 };
 http.createServer(app.callback()).listen(HTTP_PORT);
-console.log(`http://localhost:${HTTP_PORT}`);
+// console.log(`http://localhost:${HTTP_PORT}`);
 console.log('http://www.harmonious.tech');
 https.createServer(SSL, app.callback()).listen(HTTPS_PORT);
-console.log(`https://localhost:${HTTPS_PORT}`);
+// console.log(`https://localhost:${HTTPS_PORT}`);
 console.log('https://www.harmonious.tech');
-
-
-router.get('/', async (ctx) => {
-    ctx.redirect('index.html');
-});
-
-router.use('/users', users);
