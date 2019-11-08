a6f4024a9bb604099c0a5e83f2fa962d1dd03dc0
Ryzen <zilckiqgames@hotmail.com>
2019-11-08 12:03:04 +0800

    1

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,4 @@
-const btn_submit = document.getElementById('submit');
-
-btn_submit.addEventListener('click', () => {
-    const input_user = document.querySelector('.loginBox input[type = "text"]');
-    const input_passwd = document.querySelector('.loginBox input[type = "password"]');
-
-    if (input_user.validity.valid && input_passwd.validity.valid) {
-        const username = input_user.value;
-        const password = input_passwd.value;
-        alert(`用户名:${username} 密码:${password}`);
-    } else {
-        alert("填写不规范，请检查后重新输入！");
-    }
-});
+export async function send_req(url, options) {
+    const res = await fetch(url, options);
+    return await res.json();
+}
